befc6b4fe37bb8f013eb50eda61751303dc698d6
gurpreet-singh-5000 <56289528+gurpreet-singh-5000@users.noreply.github.com>
2020-04-28 18:14:50 +0530

    Update BTB.py

--- BTB.py
+++ BTB.py
@@ -1,10 +1,9 @@
-
 btb={}
 #btb['0x4']={1,'T','0xa'}
 #Each row is of the form - valid bit, Current state, taken address
 def add_instruction(pc,takenAdd,initialState): 
     if pc in btb:
-        return
+        return False
     else:
         btb[pc]=[1,initialState,takenAdd]
         #print (btb)
@@ -15,10 +14,22 @@ def update_state(pc,new_state):
     else:
         btb[pc][1]=new_state        
         return 
+def returnCurrentState(pc):
+    if pc not in btb:
+        return False
+    else:
+        return btb[pc][1]
+def returnTakenAddress(pc):
+    if pc not in btb:
+        return False
+    else:
+        return btb[pc][2]
 """add_instruction('0xa','0x4','T') 
-add_instruction('0x5','0x4','T') 
+add_instruction('0xa','0x7','T')
+add_instruction('0x5','0x6','T') 
 add_instruction('0x6','0x4','T') 
 print (btb)
 update_state('0xa','NT')
-print(btb)""" 
+print(returnTakenAddress('0x5'))
+print(btb)"""
   
